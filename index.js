7085e48cbc8286cfe9680dbca8e8ae407ffdc9a2
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-11-09 23:53:01 +0100

    Pridavam reseni vcetne druhe casti

--- index.js
+++ index.js
@@ -1,27 +1,63 @@
 const product1 = {
   image: 'images/destniklobouk.jpg',
   name: 'Deštníklobouk',
-  description: 'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
+  description:
+    'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
 };
 
 const product2 = {
   image: 'images/slepickabelka.jpg',
   name: 'Slepičkabelka',
-  description: 'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
+  description:
+    'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
 };
 
 const product3 = {
   image: 'images/anatoplavky.jpg',
   name: 'Anatoplavky',
-  description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
+  description:
+    'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
 
+// const produkt1 = document.querySelector('#product1');
+// produkt1.classList.add("border-primary")
 
-const produkt1 = document.querySelector('#product1');
-produkt1.classList.add("border-primary")
+// const button2 = document.querySelector('#product2 button');
+// button2.classList.remove("btn-primary");
 
-const button2 = document.querySelector('#product2 button');
-button2.classList.remove("btn-primary");
+// const produkt3 = document.querySelector('#product3 .card-title');
+// produkt3.classList.toggle("text-center")
 
-const produkt3 = document.querySelector('#product3 .card-title');
-produkt3.classList.toggle("text-center")
+
+const product1HTML = `
+  <img class="card-img-top" src="${product1.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+const product1Elm = document.querySelector('#product1');
+product1Elm.innerHTML = product1HTML;
+
+const product2HTML = `
+  <img class="card-img-top" src="${product2.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product2.name}</h2>
+    <p class="card-text">${product2.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+const product2Elm = document.querySelector('#product2');
+product2Elm.innerHTML = product2HTML;
+
+const product3HTML = `
+  <img class="card-img-top" src="${product3.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product3.name}</h2>
+    <p class="card-text">${product3.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+const product3Elm = document.querySelector('#product3');
+product3Elm.innerHTML = product3HTML;
